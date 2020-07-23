f059c5ec64bbf4bc13074a5188db2fe1a8beceed
Hélio Dourado <heliohdd@yahoo.com.br>
2020-07-23 11:43:08 -0300

    Service layer, component registration

--- src/main/java/com/heliohdd/webservice/resources/UserResource.java
+++ src/main/java/com/heliohdd/webservice/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.heliohdd.webservice.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.heliohdd.webservice.entities.User;
+import com.heliohdd.webservice.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-		@GetMapping
-		 public ResponseEntity<User> findAll() {
-			 User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-			 return ResponseEntity.ok().body(u);
+	@Autowired
+	private UserService userService;
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
